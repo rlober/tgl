d8698fdb2dfa3ed07af92117dfd1315def22b217
Ryan Lober <rlober@gmail.com>
2016-02-09 15:55:09 +0100

    add getDesired pure virtuals

--- include/tgl/Trajectory.hpp
+++ include/tgl/Trajectory.hpp
@@ -1,20 +1,44 @@
 #ifndef TGL_TRAJECTORY_H
 #define TGL_TRAJECTORY_H
 
+// STL includes
 #include <iostream>
+#include <vector>
+
+// Eigen includes
 #include <Eigen/Dense>
+
+// TGL includes
+#include "tgl/TglTools.hpp"
+#include "tgl/TglTypes.hpp"
+
+
+
 
 namespace tgl
 {
 
-class Trajectory {
-    public:
-        // Constructor function
-        Trajectory();
-        //Destructor
-        ~Trajectory();
+using Waypoints = std::vector<Eigen::VectorXd>;
+using WaypointTimes = std::vector<double>;
 
-    protected:
+class Trajectory {
+public:
+    // Constructor function
+    Trajectory();
+    //Destructor
+    ~Trajectory();
+
+    // Open Loop
+    virtual TglMessage getDesired(Eigen::MatrixXd& desired) = 0;
+    virtual TglMessage getDesired(const double time_step, Eigen::MatrixXd& desired) = 0;
+
+    // Closed Loop
+    virtual TglMessage getDesired(const Eigen::MatrixXd& current, Eigen::MatrixXd& desired) = 0;
+    virtual TglMessage getDesired(const double time_step, const Eigen::MatrixXd& current, Eigen::MatrixXd& desired) = 0;
+
+protected:
+    Waypoints wpts;
+    WaypointTimes wptimes;
 
 };
 
