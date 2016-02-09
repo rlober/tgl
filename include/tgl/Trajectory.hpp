de8db3432bdd8b3f5e0956a76dd09c7f29ba008b
Ryan Lober <rlober@gmail.com>
2016-02-09 13:40:10 +0100

    Removed crap and minimimalized the cmake. Compiles but does nothing.

---
+++ include/tgl/Trajectory.hpp
@@ -0,0 +1,22 @@
+#ifndef TGL_TRAJECTORY_H
+#define TGL_TRAJECTORY_H
+
+#include <iostream>
+#include <Eigen/Dense>
+
+namespace tgl
+{
+
+class Trajectory {
+    public:
+        // Constructor function
+        Trajectory();
+        //Destructor
+        ~Trajectory();
+
+    protected:
+
+};
+
+} // end of namespace tgl
+#endif // TGL_TRAJECTORY_H
