6ad014e571c8ae4a4311e8d3dd0eb6cc94e160f5
Ryan Lober <rlober@gmail.com>
2016-02-09 18:01:39 +0100

    Test class structure started.

--- tests/TglTestTools.hpp
+++ tests/TglTestTools.hpp
@@ -1,11 +1,32 @@
 #ifndef TGL_TGLTESTTOOLS_HPP
 #define TGL_TGLTESTTOOLS_HPP
 
+#include "tgl/Waypoints.hpp"
+
 namespace tgl{
-    enum TglTestMessage {
-        TGL_TEST_ERROR = -1,
-        TGL_TEST_SUCCESS = 1
-    };
+enum TglTestMessage {
+    TGL_TEST_FAILURE = 0,
+    TGL_TEST_SUCCESS = 1
+};
+static int TEST_COUNT=0; // increment for each new test case
+static int TEST_SUCCESS_COUNT=0; // Number of successful tests
+
+class WaypointTest{
+public:
+    WaypointTest(){TEST_COUNT++;}
+
+    void runTest(const Waypoints& wptObject){
+        TEST_SUCCESS_COUNT += doRunTest(wptObject);
+    }
+
+protected:
+
+    virtual TglTestMessage doRunTest(const Waypoints& wptObject) = 0;
+};
+
+
+
+
 }
 
 
--- tests/WaypointTests/main.cpp
+++ tests/WaypointTests/main.cpp
@@ -3,7 +3,31 @@
 
 using namespace tgl;
 
+class ConstructorTest : public WaypointTest{
+protected:
+    TglTestMessage doRunTest(const Waypoints& wpt){
+        std::cout << "Test 1" << std::endl;
+        return TGL_TEST_SUCCESS;
+    }
+};
+
+
+
 int main(int argc, char const *argv[]) {
 
-    return 0;
+    Waypoints wpt;
+    /*
+    *   Test Case 1
+    */
+    WaypointTest* test1 = new ConstructorTest;
+    test1->runTest(wpt);
+
+
+    if (TEST_COUNT == TEST_SUCCESS_COUNT) {
+        std::cout << "All Waypoint class tests ("<< TEST_SUCCESS_COUNT <<"/"<< TEST_COUNT <<") passed!" << std::endl;
+    }
+    else {
+        std::cout << TEST_SUCCESS_COUNT <<"/"<< TEST_COUNT << " Waypoint class tests failed:" << std::endl;
+    }
+    return TEST_COUNT == TEST_SUCCESS_COUNT;
 }
