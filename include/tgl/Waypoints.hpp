475dec3a3a61e7f4662fdc3eee63b0aaffdbb61b
Ryan Lober <rlober@gmail.com>
2016-02-10 16:01:24 +0100

    Added some function declarations - need to implement.

--- include/tgl/Waypoints.hpp
+++ include/tgl/Waypoints.hpp
@@ -9,6 +9,9 @@
 // Eigen includes
 #include <Eigen/Dense>
 
+// Glog includes
+#include <glog/logging.h>
+
 // TGL includes
 #include "tgl/TglTools.hpp"
 #include "tgl/TglTypes.hpp"
@@ -17,18 +20,44 @@
 namespace tgl
 {
 using StdVectorXd = std::vector<Eigen::VectorXd>;
+using StdDoubleVector = std::vector<double>;
 using WaypointMap = std::map<double, Eigen::VectorXd>; // Key = time_step, Value = VectorXd Waypoint
 
 class Waypoints {
 public:
-    // Constructor function
+    // Constructor functions
     Waypoints();
+    Waypoints(const StdVectorXd& wpts);
+    Waypoints(const StdVectorXd& wpts, const StdDoubleVector& wpt_times);
+
     //Destructor
     ~Waypoints();
 
-protected:
+    //Set Waypoints
+    TglMessage setWaypoints(const StdVectorXd& wpts);
+    TglMessage setWaypoints(const StdVectorXd& wpts, const StdDoubleVector& wpt_times);
+
+    //Add waypoints
+    TglMessage addWaypoint(const Eigen::VectorXd& wpt);
+    TglMessage addWaypoint(const Eigen::VectorXd& wpt, const double wpt_time);
+    TglMessage addWaypoints(const StdVectorXd& wpts);
+    TglMessage addWaypoints(const StdVectorXd& wpts, const StdDoubleVector& wpt_times);
+
+    //Eigen convertors
+    Eigen::MatrixXd asMatrix();
+
+    //Getters
+    Eigen::VectorXd getWaypointTimes();
+    double getLastWaypointTime();
+    int getWaypointDimension();
+    int getNumberOfWaypoints();
+    Eigen::VectorXd getWaypointAtTime(const double time_step);
 
 private:
+    TglMessage setWaypointMap(const StdVectorXd& wpts, const StdDoubleVector& wpt_times);
+
+
+    WaypointMap wptMap;
 
 };
 
--- src/Waypoints.cpp
+++ src/Waypoints.cpp
@@ -5,8 +5,34 @@ using namespace tgl;
 
 Waypoints::Waypoints()
 {
+    LOG(ERROR) << "This is a test.";
+}
+
+Waypoints::Waypoints(const StdVectorXd& wpts)
+{
+    if(!setWaypoints(wpts)){
+        std::cout << "error" << std::endl;
+    }
+}
+
+Waypoints::Waypoints(const StdVectorXd& wpts, const StdDoubleVector& wpt_times)
+{
+    if(!setWaypoints(wpts, wpt_times)){
+        std::cout << "error" << std::endl;
+    }
 }
 
 Waypoints::~Waypoints()
 {
+
+}
+
+TglMessage Waypoints::setWaypoints(const StdVectorXd& wpts)
+{
+    return TGL_OK;
+}
+
+TglMessage Waypoints::setWaypoints(const StdVectorXd& wpts, const StdDoubleVector& wpt_times)
+{
+    return TGL_OK;
 }
