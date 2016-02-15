ed40f87f13f3f0335e903e4aa87f492f74bb8eda
Ryan Lober <rlober@gmail.com>
2016-02-15 15:41:21 +0100

    Add comments to tools

--- include/tgl/TglTools.hpp
+++ include/tgl/TglTools.hpp
@@ -30,21 +30,34 @@
 #define TGL_TGLTOOLS_H
 #include <chrono>
 
-
+/*! \brief Chronometer START trigger.
+ *
+ *  To clock a segment of code simply add this before the segment. Note: It must be followed by a STOP trigger before a new segment can be clocked. The chronometer will use the highest fidelity possible. See [std::chrono::high_resolution_clock](http://en.cppreference.com/w/cpp/chrono/high_resolution_clock) for more details.
+ */
 #ifndef TGL_CHRONO_START
 #define TGL_CHRONO_START TGL_CHRONO_START_LINE = __LINE__; TGL_CHRONO_START_TIME = std::chrono::high_resolution_clock::now();
 #endif
 
-
+/*! \brief Chronometer STOP trigger (ms).
+ *
+ *  Add this macro to stop the code clocking. The output is written to the console and gives the file path, lines clocked and the total duration between the START and STOP triggers in milliseconds. See the other STOP triggers for different clock periods.
+ */
 #ifndef TGL_CHRONO_STOP
 #define TGL_CHRONO_STOP TGL_CHRONO_STOP_TIME = std::chrono::high_resolution_clock::now(); std::cout << "|------------|\n| tgl_chrono |\n|------------|\n" << "file: "<< __FILE__ << "\nlines: "<< TGL_CHRONO_START_LINE << "-"<< __LINE__ << "\ntime_elapsed: "<< std::chrono::duration_cast<std::chrono::milliseconds>(TGL_CHRONO_STOP_TIME - TGL_CHRONO_START_TIME).count() << " ms\n\n"; TGL_CHRONO_COUNT++;
 #endif
 
-
+/*! \brief Chronometer STOP trigger (us).
+ *
+ *  Add this macro to stop the code clocking. The output is written to the console and gives the file path, lines clocked and the total duration between the START and STOP triggers in microseconds. See the other STOP triggers for different clock periods.
+ */
 #ifndef TGL_CHRONO_STOP_US
 #define TGL_CHRONO_STOP_US TGL_CHRONO_STOP_TIME = std::chrono::high_resolution_clock::now(); std::cout << "|------------|\n| tgl_chrono |\n|------------|\n" << "file: "<< __FILE__ << "\nlines: "<< TGL_CHRONO_START_LINE << "-"<< __LINE__ << "\ntime_elapsed: "<< std::chrono::duration_cast<std::chrono::microseconds>(TGL_CHRONO_STOP_TIME - TGL_CHRONO_START_TIME).count() << " us\n\n"; TGL_CHRONO_COUNT++;
 #endif
 
+/*! \brief Chronometer STOP trigger (ns).
+ *
+ *  Add this macro to stop the code clocking. The output is written to the console and gives the file path, lines clocked and the total duration between the START and STOP triggers in nanoseconds. See the other STOP triggers for different clock periods.
+ */
 #ifndef TGL_CHRONO_STOP_NS
 #define TGL_CHRONO_STOP_NS TGL_CHRONO_STOP_TIME = std::chrono::high_resolution_clock::now(); std::cout << "|------------|\n| tgl_chrono |\n|------------|\n" << "file: "<< __FILE__ << "\nlines: "<< TGL_CHRONO_START_LINE << "-"<< __LINE__ << "\ntime_elapsed: "<< std::chrono::duration_cast<std::chrono::nanoseconds>(TGL_CHRONO_STOP_TIME - TGL_CHRONO_START_TIME).count() << " ns\n\n"; TGL_CHRONO_COUNT++;
 #endif
@@ -52,9 +65,10 @@
 namespace tgl
 {
 
-static int TGL_CHRONO_COUNT = 0;
-static int TGL_CHRONO_START_LINE = 0;
-static std::chrono::high_resolution_clock::time_point TGL_CHRONO_START_TIME, TGL_CHRONO_STOP_TIME;
+static int TGL_CHRONO_COUNT = 0;                                                /*!< A counter used to measure the number of segments clocked. TODO: use this to provide an ID for the clock results which can be pulled from a log written to file. */
+static int TGL_CHRONO_START_LINE = 0;                                           /*!< The line where the START trigger was called. */
+static std::chrono::high_resolution_clock::time_point TGL_CHRONO_START_TIME;    /*!< System time at the START trigger. */
+static std::chrono::high_resolution_clock::time_point TGL_CHRONO_STOP_TIME;     /*!< System time at the START trigger. */
 
 } // End of namespace tgl
 #endif //TGL_TGLTOOLS_H
